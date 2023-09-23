08b8b8b858132e833aeb4068dbd619a66b6cac1d
Ty <valkryst@valkryst.com>
2023-09-23 17:29:21 -0300

    Update JavaDoc descriptions

--- src/main/java/com/valkryst/JPathList/RecursionMode.java
+++ src/main/java/com/valkryst/JPathList/RecursionMode.java
@@ -4,10 +4,10 @@ public enum RecursionMode {
     /** Include all paths, but do not recurse directories. */
     NONE,
 
-    /** Include all file paths. */
+    /** Include all file paths and recurse directories. */
     FILES_ONLY,
 
-    /** Include all directory paths. */
+    /** Include all directory paths and recurse directories. */
     DIRECTORIES_ONLY,
 
     /** Include all paths and recurse directories. */
